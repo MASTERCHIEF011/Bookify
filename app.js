07db8824597683c09031b99adc6c96a2f0a088b9
Harsh Pandey <harshpandey011@gmail.com>
2020-08-21 02:20:54 +0530

    changes made

--- app.js
+++ app.js
@@ -6,23 +6,6 @@ var app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","html");
 app.use(express.static(path.join(__dirname, './public')));
-//app.use('/stylesheets', express.static(__dirname + '/stylesheets'));
-// app.get("/",function(req,res){
-//     res.sendFile(path.join(__dirname, "/public","/login.html"));
-// })
-// app.post("/",function(req,res){
-//     var userid=req.body.username;
-//     var passcode=req.body.password;
-//     if(userid==="admin" && passcode==="admin"){
-//         res.redirect("/home");
-//     }
-//     else{
-//         res.redirect("/");
-//     }
-// });
-// app.get("/home",function(req,res){
-//     res.sendFile(path.join(__dirname, "/public","/home.html"));
-// });
 app.use(userroutes);
 app.listen(3000,function(){
     console.log("server has started");
