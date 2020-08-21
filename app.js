d96c5709385013d0ac39096f3c824ebe087d1fd2
Harsh Pandey <harshpandey011@gmail.com>
2020-08-22 04:13:16 +0530

    included passport

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express=require('express');
 var bodyParser =require('body-parser');
-var userroutes =require('./routes/routes.js');
+var userroutes =require('./landingPage/routes.js');
 var path= require("path");
 var app=express();
+const passport=require('passport')
+const initialisePassport=require('./passport-config');
+//initialisePassport(passport)
 app.use(bodyParser.urlencoded({extended:true}));
-app.set("view engine","html");
-app.use(express.static(path.join(__dirname, './public')));
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname, './views')));
 app.use(userroutes);
 app.listen(3000,function(){
     console.log("server has started");
