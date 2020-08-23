9b3681db050094776c2af6d841b4e92b3f056c0c
Harsh Pandey <harshpandey011@gmail.com>
2020-08-23 16:33:28 +0530

    made several changes

---
+++ public/authenticate_session.js
@@ -0,0 +1,18 @@
+var passport=require('passport')
+module.exports={
+ checkAuthenticated: function(req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/login')
+
+},
+ checkNotAuthenticated: function(req,res,next){
+    if(req.isAuthenticated()){
+      return res.redirect('/')
+    }
+    next()
+
+}
+
+};
