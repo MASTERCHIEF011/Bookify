853703fa61af9ae8390464aee163c21509efc4fa
Harsh Pandey <harshpandey011@gmail.com>
2020-09-02 22:00:48 +0530

    changes made to app

--- app.js
+++ app.js
@@ -9,16 +9,12 @@ const session=require('express-session')
 const passport=require('passport')
 const path= require("path")
 const methodOverride=require('method-override')
-const landingroutes =require('./routes/landing_routes.js');
-const homeroutes =require('./routes/home_routes.js');
-const authenticate_session=require('./public/authenticate_session.js')
-const initializePassport=require('./passport-config')
-const newuser =require('./public/database.js')
-initializePassport(
-    passport,
-    username => users.find(user =>user.username===username),
-    id=>users.find(user=>user.id===id)
-    ) 
+
+
+const landingRoutes =require('./routes/landing_routes.js')
+const homeRoutes =require('./routes/home_routes.js')
+const passportConfig=require('./passport-config.js')
+
 app.set("view engine","ejs")
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(flash())
@@ -30,10 +26,11 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname, './views')));
-app.use(landingroutes)
-app.use(homeroutes)
-const users=newuser.users
+app.use(express.static(path.join(__dirname, './views')))
+
+
+app.use(landingRoutes)
+app.use(homeRoutes)
 
 app.listen(3000,function(){
     console.log("server has started")
