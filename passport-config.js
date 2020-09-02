c46448062438ece0d015990dfe446ecc4261a2e5
Harsh Pandey <harshpandey011@gmail.com>
2020-09-02 22:01:26 +0530

    changes made to passport config

--- passport-config.js
+++ passport-config.js
@@ -1,26 +1,40 @@
 const LocalStrategy = require('passport-local').Strategy
+const passport=require('passport')
 const bcrypt=require('bcrypt')
-function initialize(passport,getUserByUsername,getUserById){
-    const authenticateUser= async(username,password,done) =>{
-        const user=getUserByUsername(username)
-        if(user==null){
+const fetch= require('./database/fetch_data.js')
+
+
+  function authenticateUser(username,password,done){
+    fetch.fetch_user (username,async function(err,rows){
+        if (err){
+            return done(err)
+        }
+        if (rows[0]==null) {
             return done(null,false,{message:" doesn't exsist!"})
         }
         try {
-            if(await bcrypt.compare(password,user.password)){
-                return done(null,user)
+            if(await bcrypt.compare(password,rows[0].password)){
+                return done(null,rows)
             }
             else {
+                console.log("hi")
                 return done(null,false,{message: ' Incorrect!'})
             }
         } catch (e){
             return done(e)
         }
     }
-    passport.use(new LocalStrategy({usernameField:'username'},authenticateUser))
-    passport.serializeUser((user,done) => done(null,user.id))
+    )
+    }
+
+    
+    passport.use(new LocalStrategy(authenticateUser))
+    passport.serializeUser((rows,done) => done(null,rows[0].id))
     passport.deserializeUser((id,done) => {
-        return done(null,getUserById(id))
+        fetch.fetch_id(id,function(err,rows){
+            return done(null,rows)
+        })
+        
     })
-}
-module.exports=initialize
+
+ module.exports=authenticateUser
