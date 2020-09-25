29e15ebed1f83c3ec772a17ed58053caa08534f7
Harsh Pandey <harshpandey011@gmail.com>
2020-09-26 02:05:33 +0530

    added function for books etc

--- database/fetch_data.js
+++ database/fetch_data.js
@@ -27,5 +27,38 @@ const connection = mysql.createConnection({
           else
           callback(true,null)
       })
+  },
+  fetch_all_books :function(callback){
+    const query="select * from books"
+    connection.query(query,(err,rows)=>{
+      if (!err) {
+        callback(null,rows)
+      }
+      else
+      callback(true,null)
+    })
+
+  },
+  fetch_all_magazines :function(callback){
+    const query="select * from magazines"
+    connection.query(query,(err,rows)=>{
+      if (!err) {
+        callback(null,rows)
+      }
+      else
+      callback(true,null)
+    })
+
+  },
+  fetch_all_stamps :function(callback){
+    const query="select * from stamps"
+    connection.query(query,(err,rows)=>{
+      if (!err) {
+        callback(null,rows)
+      }
+      else
+      callback(true,null)
+    })
+
   }
   }
